b0f5619fa592347ee40a297f0594d16b96b12fa0
duchenpaul <duchenpaul@gmail.com>
2019-11-10 17:57:54 +0800

    Correct config

--- create_docker.sh
+++ create_docker.sh
@@ -1,9 +1,9 @@
-APP_NAME=homebridge_midware_app
+APP_NAME=home-assistant-control
 
 cat>Dockerfile<<EOF
 FROM python:3.8
 ADD ${APP_NAME} /app
-WORKDIR /app/FlaskApp
+WORKDIR /app
 RUN pip install --trusted-host pypi.douban.com -i http://pypi.douban.com/simple -r /app/requirements.txt 
 RUN pip install --trusted-host pypi.douban.com -i http://pypi.douban.com/simple gunicorn
 EXPOSE 5000
@@ -17,7 +17,7 @@ mkdir -p ${HOST_EXCHANGE_DIR}
 sudo docker build --tag ${APP_NAME} .
 sudo docker save ${APP_NAME} | gzip -c > ${APP_NAME}.tar.gz
 
-APP_NAME=homebridge_midware_app
+APP_NAME=home-assistant-control
 # sudo docker load < ${APP_NAME}.tar
 sudo docker images ${APP_NAME}
 sudo docker run -p 5050:5000 --name ${APP_NAME}_ins -v ${HOST_EXCHANGE_DIR}:${CONTAINER_EXCHANGE_DIR} ${APP_NAME}
